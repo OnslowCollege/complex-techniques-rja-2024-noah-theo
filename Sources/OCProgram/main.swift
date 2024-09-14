2cb3b70f984b42afb7b0b1e73321d1a1140b612b
Theo Living <theo.living@student.onslow.school.nz>
2024-09-14 03:42:16 +0000

    Sep 14, 2024, 3:42 AM

--- Sources/OCProgram/main.swift
+++ Sources/OCProgram/main.swift
@@ -423,6 +423,7 @@ class BlackJackApp : OCApp{
 
 
     override open func main(app: any OCAppDelegate) -> OCControl {
+        close()
         hitButton.enabled = false
         standButton.enabled = false
         insuranceButton.enabled = false
